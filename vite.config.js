80d233738b9a33c69293f6546d7f0508d6a1565a
김종필 <jrussi0225@gmail.com>
2025-12-19 01:57:53 +0900

    Fix: Application failed to respond error

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 모든 외부 접속 허용
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: true
+  }
+})
